65d70de6a70c59213118f962203e8622c0f2b1f3
Antonio Ramirez <amigo.cobos@gmail.com>
2014-05-08 10:19:56 +0200

    update to PSR1-2

--- Taggable.php
+++ Taggable.php
@@ -17,45 +17,45 @@ use yii\db\Query;
  */
 class Taggable extends Behavior
 {
-	/**
-	 * @var ActiveRecord the owner of this behavior.
-	 */
-	public $owner;
-	/**
-	 * @var string
-	 */
-	public $attribute = 'tagNames';
-	/**
-	 * @var string
-	 */
-	public $name = 'name';
-	/**
-	 * @var string
-	 */
-	public $frequency = 'frequency';
-	/**
-	 * @var string
-	 */
-	public $relation = 'tags';
+    /**
+     * @var ActiveRecord the owner of this behavior.
+     */
+    public $owner;
+    /**
+     * @var string
+     */
+    public $attribute = 'tagNames';
+    /**
+     * @var string
+     */
+    public $name = 'name';
+    /**
+     * @var string
+     */
+    public $frequency = 'frequency';
+    /**
+     * @var string
+     */
+    public $relation = 'tags';
 
-	/**
-	 * @inheritdoc
-	 */
-	public function events()
-	{
-		return [
-			ActiveRecord::EVENT_AFTER_FIND => 'afterFind',
-			ActiveRecord::EVENT_AFTER_INSERT => 'afterSave',
-			ActiveRecord::EVENT_AFTER_UPDATE => 'afterSave',
-			ActiveRecord::EVENT_BEFORE_DELETE => 'beforeDelete',
-		];
-	}
+    /**
+     * @inheritdoc
+     */
+    public function events()
+    {
+        return [
+            ActiveRecord::EVENT_AFTER_FIND => 'afterFind',
+            ActiveRecord::EVENT_AFTER_INSERT => 'afterSave',
+            ActiveRecord::EVENT_AFTER_UPDATE => 'afterSave',
+            ActiveRecord::EVENT_BEFORE_DELETE => 'beforeDelete',
+        ];
+    }
 
-	/**
-	 * @param Event $event
-	 */
-	public function afterFind($event)
-	{
+    /**
+     * @param Event $event
+     */
+    public function afterFind($event)
+    {
         $items = [];
 
         foreach ($this->owner->{$this->relation} as $tag) {
@@ -65,88 +65,88 @@ class Taggable extends Behavior
         $this->owner->{$this->attribute} = is_array($this->owner->{$this->attribute})
             ? $items
             : implode(', ', $items);
-	}
+    }
 
-	/**
-	 * @param Event $event
-	 */
-	public function afterSave($event)
-	{
-		if ($this->owner->{$this->attribute} === null) {
-			return;
-		}
+    /**
+     * @param Event $event
+     */
+    public function afterSave($event)
+    {
+        if ($this->owner->{$this->attribute} === null) {
+            return;
+        }
 
-		if (!$this->owner->getIsNewRecord()) {
-			$this->beforeDelete($event);
-		}
+        if (!$this->owner->getIsNewRecord()) {
+            $this->beforeDelete($event);
+        }
 
-		$names = array_unique(preg_split(
-			'/\s*,\s*/u',
-			preg_replace(
-				'/\s+/u',
-				' ',
-				is_array($this->owner->{$this->attribute})
-					? implode(',', $this->owner->{$this->attribute})
-					: $this->owner->{$this->attribute}
-			),
-			-1,
-			PREG_SPLIT_NO_EMPTY
-		));
+        $names = array_unique(preg_split(
+            '/\s*,\s*/u',
+            preg_replace(
+                '/\s+/u',
+                ' ',
+                is_array($this->owner->{$this->attribute})
+                    ? implode(',', $this->owner->{$this->attribute})
+                    : $this->owner->{$this->attribute}
+            ),
+            -1,
+            PREG_SPLIT_NO_EMPTY
+        ));
 
-		$relation = $this->owner->getRelation($this->relation);
-		$pivot = $relation->via->from[0];
-		/** @var ActiveRecord $class */
-		$class = $relation->modelClass;
-		$rows = [];
+        $relation = $this->owner->getRelation($this->relation);
+        $pivot = $relation->via->from[0];
+        /** @var ActiveRecord $class */
+        $class = $relation->modelClass;
+        $rows = [];
 
-		foreach ($names as $name) {
-			$tag = $class::findOne([$this->name => $name]);
+        foreach ($names as $name) {
+            $tag = $class::findOne([$this->name => $name]);
 
-			if ($tag === null) {
-				$tag = new $class();
-				$tag->{$this->name} = $name;
-			}
+            if ($tag === null) {
+                $tag = new $class();
+                $tag->{$this->name} = $name;
+            }
 
-			$tag->{$this->frequency}++;
+            $tag->{$this->frequency}++;
 
-			if (!$tag->save()) {
-				continue;
-			}
+            if (!$tag->save()) {
+                continue;
+            }
 
-			$rows[] = [$this->owner->getPrimaryKey(), $tag->getPrimaryKey()];
-		}
+            $rows[] = [$this->owner->getPrimaryKey(), $tag->getPrimaryKey()];
+        }
 
-		if (!empty($rows)) {
-			$this->owner->getDb()
-				->createCommand()
-				->batchInsert($pivot, [key($relation->via->link), current($relation->link)], $rows)
-				->execute();
-		}
-	}
+        if (!empty($rows)) {
+            $this->owner->getDb()
+                ->createCommand()
+                ->batchInsert($pivot, [key($relation->via->link), current($relation->link)], $rows)
+                ->execute();
+        }
+    }
 
-	/**
-	 * @param Event $event
-	 */
-	public function beforeDelete($event)
-	{
-		$relation = $this->owner->getRelation($this->relation);
-		$pivot = $relation->via->from[0];
-		/** @var ActiveRecord $class */
-		$class = $relation->modelClass;
-		$query = new Query();
-		$pks = $query
-			->select(current($relation->link))
-			->from($pivot)
-			->where([key($relation->via->link) => $this->owner->getPrimaryKey()])
-			->column($this->owner->getDb());
+    /**
+     * @param Event $event
+     */
+    public function beforeDelete($event)
+    {
+        $relation = $this->owner->getRelation($this->relation);
+        $pivot = $relation->via->from[0];
+        /** @var ActiveRecord $class */
+        $class = $relation->modelClass;
+        $query = new Query();
+        $pks = $query
+            ->select(current($relation->link))
+            ->from($pivot)
+            ->where([key($relation->via->link) => $this->owner->getPrimaryKey()])
+            ->column($this->owner->getDb());
 
-		if (!empty($pks)) {
-			$class::updateAllCounters([$this->frequency => -1], ['in', $class::primaryKey(), $pks]);
-		}
+        if (!empty($pks)) {
+            $class::updateAllCounters([$this->frequency => -1], ['in', $class::primaryKey(), $pks]);
+        }
 
-		$this->owner->getDb()
-			->createCommand()
-			->delete($pivot, [key($relation->via->link) => $this->owner->getPrimaryKey()])
-			->execute();
-	}
+        $this->owner->getDb()
+            ->createCommand()
+            ->delete($pivot, [key($relation->via->link) => $this->owner->getPrimaryKey()])
+            ->execute();
+    }
 }
